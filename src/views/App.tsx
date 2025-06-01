6486976c33e9db0b0f11390381a6a0fe358feb9f
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-06-01 14:52:01 +0200

    added fetch simulating

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,13 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Expense } from "../types/Expense";
 import ExpenseForm from "../components/ExpenseForm";
 import ExpenseList from "../components/ExpenseList";
 
 function App() {
   const [expenses, setExpenses] = useState<Expense[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  const handleAddExpense = (expense: Expense) => {
-    setExpenses((prev) => [expense, ...prev]);
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      setLoading(true);
+      try {
+        const res = await new Promise<Expense[]>((resolve) =>
+          setTimeout(() => {
+            resolve([
+              {
+                id: "1",
+                title: "Кава",
+                amount: 40,
+                category: "food",
+                date: "2025-05-26",
+              },
+              {
+                id: "2",
+                title: "Проїзд",
+                amount: 80,
+                category: "transport",
+                date: "2025-05-25",
+              },
+            ]);
+          }, 1000)
+        );
+        setExpenses(res);
+      } catch (error) {
+        console.error("Error", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchExpenses();
+  }, []);
+
+  const handleAddExpense = async (expense: Expense) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      setExpenses((prev) => [expense, ...prev]);
+    } catch (error) {
+      console.error("Error", error);
+    }
   };
 
   return (
